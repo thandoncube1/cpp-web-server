b99c2257e638a42380a2e9d53a0bcf0bf4f641bb
thandoncube1 <ncubethando20@gmail.com>
2024-09-15 01:25:41 -0400

    Adding the index html file.

--- request.cpp
+++ request.cpp
@@ -1,12 +1,7 @@
 #include"request.h"
 
 void HttpRequest::parseRequest(const std::string& rawRequest){
-    //Todo 
-    //raw request se Get method se path nikalkr map me daalna hai.
-    //uss path me rakhi file se html read krna hai.(hope i will make a new method for this.)
-    //ab html file se content read krne ke baad string stream me store krna hai.
-    //aur sara content ko http response ke format me frame krke browser ko bhejna hai.
-    //HAHAHAHA HAHAHAHAHAH
+    //Todo
     int currindex=0;
     while(currindex < rawRequest.length()){
         if(rawRequest[currindex] == ' '){
@@ -26,43 +21,30 @@ void HttpRequest::parseRequest(const std::string& rawRequest){
         path += rawRequest[currindex];
         currindex++;
     }
-   
+
     headers["path"] = path;
-    
-
-
-    //mujhe ab method mil gya hai and path mil gya hai ab mai path me se file ka name nikaalunga aur 
-    //html file ko open karke uska content read karunga.
-    //iske liye mai ek methon banaunga readhtmlFile().
-
-    
-
- 
-
 
 }
 std::string HttpRequest::readHtmlFile(const std::string &path){
     //TODO
-    //ab html file me se content read krna hai.
-    //iske liye hume fstream use krna padega.
+    // Serving files to the FE.
+    // Get the file in the public directory and serve to the frontend.
     std::string filename = path.substr(1,path.length());
-   
-    std::ifstream file("../"+filename); //ifstream used for reading file if exists.
+    
+    std::cout << filename << std::endl;
+
+    std::ifstream file("./public/"+filename); //ifstream used for reading file if exists.
     int flag=0;
     if(!file){
         flag=1;
         std::cerr<<"File not found."<<std::endl;
-      
-        
     }
-
     // std::cout<<"file found"<<std::endl;
     if(flag==0){
         std::stringstream buffer;
         buffer << file.rdbuf();
         return buffer.str();
     }
-
 
     return "";
 }
